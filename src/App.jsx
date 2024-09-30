fb5f8a4edfe36986950343f0fa6109e3bae6887a
april-ns <aprilyani.safitri@gmail.com>
2024-09-30 14:35:05 +0700

    ubah teks di app

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Aprilyani NS</h1>
+      <h3>Aprilyani NS</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
